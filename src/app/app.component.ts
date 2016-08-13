846cd25bc28e7a5edfd4721ae823d3094c28520c
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2016-08-13 23:56:28 +0700

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import '../../public/css/styles.css';
+@Component({
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+    foo: _ => {
+        return 'test';
+}
+
+}
